3d9462b8168a085482a122192b9e3e7374825ae8
DeV <pateldev081999@gmail.com>
2024-07-25 23:42:45 -0400

    hero/nav/template 2 chnages

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -41,10 +41,11 @@ const Navbar = () => {
         <div 
           className={`absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white  md:mt-0 md:p-0 md:top-0 md:relative md:opacity-100 md:translate-x-0 md:flex md:items-center md:justify-between ${isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full'}`}
         >
-          <div className="flex flex-col px-2 -mx-4 md:flex-row md:mx-10 md:py-0">
-            <Link href="/" className="flex px-2.5 py-2 text-gray-700 transition-colors duration-300 transform rounded-lg  hover:scale-105 hover:text-primary md:mx-2 items-center justify-center"><span><HomeIcon className='w-6 h-6 mr-2'/></span></Link>
+          <div className="flex flex-col px-2 -mx-4 md:flex-row md:mx-10 md:py-0 items-center justify-end right-2 w-full">
+            
             <Link href="/" className="px-2.5 py-2 text-gray-700 transition-colors duration-300 transform rounded-lg  hover:scale-105 hover:text-primary  md:mx-2">About</Link>
             <Link href="/" className="px-2.5 py-2 text-gray-700 transition-colors duration-300 transform rounded-lg  hover:scale-105 hover:text-primary md:mx-2">Contact</Link>
+            <Link href="/" className="flex px-2.5 py-2 text-gray-700 transition-colors duration-300 transform rounded-lg  hover:scale-105 hover:text-primary md:mx-2 items-center justify-center"><span><HomeIcon className='w-6 h-6 mr-2'/></span></Link>
           </div>
           
         </div>

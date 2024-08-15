ea79aac61294797872cfdcb1a07837198461b145
DeV <pateldev081999@gmail.com>
2024-08-14 22:25:45 -0400

    add template 3/ deploy fix

--- app/(route)/designs/1/page.jsx
+++ app/(route)/designs/1/page.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import 'prismjs/themes/prism.css';
 import DesignComponent from '../components/PrivewTemplate';
 import {snippetData} from '@/lib/data/SnippetData'
+import GoBackLink from '../components/GoBackLink';
 
 const Design1 = () => {
   const { previewPath, htmlCodeSnippet, reactCodeSnippet } = snippetData.design1;
@@ -11,11 +12,15 @@ const Design1 = () => {
   console.log('Design 1 Data: ' + snippetData.design1)
 
   return (
+    <>
+    <GoBackLink />
+   
     <DesignComponent
       previewPath={previewPath}
       htmlCodeSnippet={htmlCodeSnippet}
       reactCodeSnippet={reactCodeSnippet}
     />
+     </>
   );
 };
 
--- app/(route)/designs/2/page.jsx
+++ app/(route)/designs/2/page.jsx
@@ -4,16 +4,20 @@ import React from 'react';
 import 'prismjs/themes/prism.css';
 import DesignComponent from '../components/PrivewTemplate';
 import {snippetData} from '@/lib/data/SnippetData'
+import GoBackLink from '../components/GoBackLink';
 
 const Design2 = () => {
   const { previewPath, htmlCodeSnippet, reactCodeSnippet } = snippetData.design2;
 
   return (
+    <>
+    <GoBackLink />
     <DesignComponent
       previewPath={previewPath}
       htmlCodeSnippet={htmlCodeSnippet}
       reactCodeSnippet={reactCodeSnippet}
     />
+    </>
   );
 };
 

360efcc497454f68486167c228e71cce5acedfdb
DeV <pateldev081999@gmail.com>
2024-09-04 11:43:42 -0400

    update(next.config): deploy fix

--- app/(route)/designs/5/page.jsx
+++ app/(route)/designs/5/page.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import D5Navbar from './compenets/D5Navbar';
-import D5HeroSection from './compenets/D5HERROS';
-import D5ThreeBoxSection from './compenets/D5ThreeBoxSection';
-import D5SocialMediaIcons from './compenets/D5SocialMediaIcons';
-import D5JoinUs from './compenets/D5JoinUs';
-import D5UpcomingEvents from './compenets/D5UpcomingEvents';
-import D5Footer from './compenets/D5footer';
-import D5LatestNews from './compenets/D5LatestNew';
-import GoBackLink from '../components/GoBackLink';
+import React from "react";
+import D5Navbar from "./compenets/D5Navbar";
+import D5HeroSection from "./compenets/D5HERROS";
+import D5ThreeBoxSection from "./compenets/D5ThreeBoxSection";
+import D5SocialMediaIcons from "./compenets/D5SocialMediaIcons";
+import D5JoinUs from "./compenets/D5JoinUs";
+import D5UpcomingEvents from "./compenets/D5UpcomingEvents";
+import D5Footer from "./compenets/D5footer";
+import D5LatestNews from "./compenets/D5LatestNew";
+import GoBackLink from "../components/GoBackLink";
 
 const DesignFive = () => {
   return (
@@ -23,6 +23,6 @@ const DesignFive = () => {
       <D5Footer />
     </div>
   );
-}
+};
 
 export default DesignFive;

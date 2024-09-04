909e7dac327650ec340c70c6dfbaeecb24f1a3fd
DeV <pateldev081999@gmail.com>
2024-09-04 11:48:06 -0400

    update(next.config): deploy fix

--- app/(route)/designs/5/page.jsx
+++ app/(route)/designs/5/page.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import D5Navbar from "./compenets/D5Navbar";
-import D5HeroSection from "./compenets/D5HERROS";
-import D5ThreeBoxSection from "./compenets/D5ThreeBoxSection";
-import D5SocialMediaIcons from "./compenets/D5SocialMediaIcons";
-import D5JoinUs from "./compenets/D5JoinUs";
-import D5UpcomingEvents from "./compenets/D5UpcomingEvents";
-import D5Footer from "./compenets/D5footer";
-import D5LatestNews from "./compenets/D5LatestNew";
+import D5Navbar from "./components/D5Navbar";
+import D5HeroSection from "./components/D5HERROS";
+import D5ThreeBoxSection from "./components/D5ThreeBoxSection";
+import D5SocialMediaIcons from "./components/D5SocialMediaIcons";
+import D5JoinUs from "./components/D5JoinUs";
+import D5UpcomingEvents from "./components/D5UpcomingEvents";
+import D5Footer from "./components/D5footer";
+import D5LatestNews from "./components/D5LatestNew";
 import GoBackLink from "../components/GoBackLink";
 
 const DesignFive = () => {

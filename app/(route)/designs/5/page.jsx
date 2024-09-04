f8cb06134ecdcb51ae923bd9c8bd76a5f8e765da
DeV <pateldev081999@gmail.com>
2024-09-04 12:01:06 -0400

    update(next.config): deploy fix

--- app/(route)/designs/5/page.jsx
+++ app/(route)/designs/5/page.jsx
@@ -3,11 +3,11 @@ import D5Navbar from "./components/D5Navbar";
 import D5HeroSection from "./components/D5HERROS";
 import D5ThreeBoxSection from "./components/D5ThreeBoxSection";
 import D5SocialMediaIcons from "./components/D5SocialMediaIcons";
-import D5JoinUs from "./components/D5JoinUs";
 import D5UpcomingEvents from "./components/D5UpcomingEvents";
 import D5Footer from "./components/D5footer";
 import D5LatestNews from "./components/D5LatestNew";
 import GoBackLink from "../components/GoBackLink";
+import D5JoinUs from "./components/D5JoinUs";
 
 const DesignFive = () => {
   return (

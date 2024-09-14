7b7e5d82c5689e649535012b9cfd57613fe6d813
DeV <pateldev081999@gmail.com>
2024-09-13 21:01:45 -0400

    update(Design 5): optimized images and improved design elemets

--- app/(route)/designs/5/components/Design5UpcomingEvents.jsx
+++ app/(route)/designs/5/components/Design5UpcomingEvents.jsx
@@ -1,45 +1,49 @@
 //compenets/UpcomingEvents.js
 export default function Design5UpcomingEvents() {
-    const events = [
-      {
-        date: "Sun, Sep 23",
-        title: "Open House & Treat",
-        location: "San Francisco",
-      },
-      {
-        date: "Tue, Nov 20",
-        title: "Weight Lifting",
-        location: "San Francisco",
-      },
-      {
-        date: "Wed, Dec 19",
-        title: "Veternes Family Lunch",
-        location: "San Francisco",
-      },
-    ];
-  
-    return (
-      <div id="upcoming-events" className="px-4 md:px-20 py-16 bg-gray-100">
-        <h2 className="text-4xl md:text-3xl font-bold text-center mb-8 text-gray-800">Upcoming Events</h2>
-        <div className="space-y-6">
-          {events.map((event, index) => (
-            <div
-              key={index}
-              className="flex flex-col md:flex-row justify-between items-start border-b pb-4"
-            >
-              <div className="text-left md:text-left">
-                <p className="text-gray-500">{event.date}</p>
-                <p className="text-lg font-semibold text-gray-800">{event.title}</p>
-                <p className="text-gray-500">{event.location}</p>
-              </div>
-              <div className="mt-4 md:mt-0">
-                <button className="bg-blue-500 text-white py-2 px-6 rounded hover:bg-blue-600 transition duration-200">
-                  Register Now
-                </button>
-              </div>
+  const events = [
+    {
+      date: "Sun, Sep 23",
+      title: "Open House & Treat",
+      location: "San Francisco",
+    },
+    {
+      date: "Tue, Nov 20",
+      title: "Weight Lifting",
+      location: "San Francisco",
+    },
+    {
+      date: "Wed, Dec 19",
+      title: "Veternes Family Lunch",
+      location: "San Francisco",
+    },
+  ];
+
+  return (
+    <div id="upcoming-events" className="px-4 md:px-20 py-16 bg-gray-100">
+      <h2 className="text-4xl md:text-3xl font-bold text-center mb-8 text-gray-800">
+        Upcoming Events
+      </h2>
+      <div className="space-y-6">
+        {events.map((event, index) => (
+          <div
+            key={index}
+            className="flex flex-col md:flex-row justify-between items-start border-b pb-4 max-w-2xl mx-auto"
+          >
+            <div className="text-left md:text-left">
+              <p className="text-gray-500">{event.date}</p>
+              <p className="text-lg font-semibold text-gray-800">
+                {event.title}
+              </p>
+              <p className="text-gray-500">{event.location}</p>
             </div>
-          ))}
-        </div>
+            <div className="mt-4 md:mt-0">
+              <button className="bg-blue-500 text-white py-2 px-6 rounded hover:bg-blue-600 transition duration-200">
+                Register Now
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
-    );
+    </div>
+  );
 }
--- app/(route)/designs/5/components/Design5WellnessProgram.jsx
+++ app/(route)/designs/5/components/Design5WellnessProgram.jsx
@@ -2,98 +2,100 @@
 
 export default function Design5WellnessProgram() {
   return (
-    <div className="bg-blue-500">
-      <div id="programs" className="pb-10 text-white pt-10">
-        <div className="container mx-auto text-center">
-          <h1 className="text-4xl font-bold">Wellness Programs</h1>
-          <p className="mt-4 text-lg">
-            Empowering Veterans through Comprehensive Health and Performance
-            Programs
-          </p>
+    <div className="bg-blue-500 ">
+      <div className="flex-col items-center justify-center">
+        <div id="programs" className="pb-10 text-white pt-10">
+          <div className="container mx-auto text-center">
+            <h1 className="text-4xl font-bold">Wellness Programs</h1>
+            <p className="mt-4 text-lg">
+              Empowering Veterans through Comprehensive Health and Performance
+              Programs
+            </p>
+          </div>
         </div>
+
+        <main className="container mx-auto py-16 px-4 max-w-screen-xl ">
+          <section className="mb-16">
+            <h2 className="text-3xl font-semibold mb-8 text-white">
+              Our Approach
+            </h2>
+            <p className="text-lg text-white">
+              At Liberty Veterans Foundation, we believe in a holistic approach
+              to health and performance. Our programs are designed to address
+              both physical and mental well-being to ensure that veterans
+              receive the comprehensive care they deserve. Our team of experts
+              works closely with each individual to develop personalized plans
+              that enhance their overall quality of life.
+            </p>
+          </section>
+
+          <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-16 ">
+            <div className=" rounded-lg overflow-hidden shadow-lg bg-gray-100">
+              <img
+                src="/D5/16.jpg"
+                alt="Health Program"
+                className="w-full h-48 object-cover"
+              />
+              <div className="p-6">
+                <h3 className="text-xl font-semibold text-gray-700">
+                  Physical Health Program
+                </h3>
+                <p className="text-gray-600 mt-2">
+                  Our physical health program focuses on improving fitness
+                  levels, providing nutritional guidance, and offering
+                  therapeutic services to help veterans regain and maintain
+                  their physical health.
+                </p>
+              </div>
+            </div>
+
+            <div className="rounded-lg overflow-hidden shadow-lg bg-gray-100">
+              <img
+                src="/D5/17.jpg"
+                alt="Mental Health Support"
+                className="w-full h-48 object-cover"
+              />
+              <div className="p-6">
+                <h3 className="text-xl font-semibold text-gray-700">
+                  Mental Health Support
+                </h3>
+                <p className="text-gray-600 mt-2">
+                  We provide mental health support through counseling, therapy,
+                  and stress management techniques to ensure veterans have the
+                  resources they need for mental well-being.
+                </p>
+              </div>
+            </div>
+
+            <div className="rounded-lg overflow-hidden shadow-lg bg-gray-100">
+              <img
+                src="/D5/15.jpg"
+                alt="Community Engagement"
+                className="w-full h-48 object-cover"
+              />
+              <div className="p-6">
+                <h3 className="text-xl font-semibold text-gray-700">
+                  Community Engagement
+                </h3>
+                <p className="text-gray-600 mt-2">
+                  Our community engagement initiatives are designed to build a
+                  supportive network, foster camaraderie, and create
+                  opportunities for veterans to connect and thrive.
+                </p>
+              </div>
+            </div>
+          </section>
+
+          <section className="text-center">
+            <a
+              href="#contact"
+              className="bg-gray-100 text-blue-500 py-3 px-6 rounded-md hover:bg-gray-200 hover:text-blue-600 transition duration-300"
+            >
+              <span>Get In Touch</span>
+            </a>
+          </section>
+        </main>
       </div>
-
-      <main className="container mx-auto py-16 px-4">
-        <section className="mb-16">
-          <h2 className="text-3xl font-semibold mb-8 text-gray-100">
-            Our Approach
-          </h2>
-          <p className="text-lg text-gray-200">
-            At Liberty Veterans Foundation, we believe in a holistic approach to
-            health and performance. Our programs are designed to address both
-            physical and mental well-being to ensure that veterans receive the
-            comprehensive care they deserve. Our team of experts works closely
-            with each individual to develop personalized plans that enhance
-            their overall quality of life.
-          </p>
-        </section>
-
-        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-16 ">
-          <div className="border border-gray-200 rounded-lg overflow-hidden shadow-lg bg-gray-100">
-            <img
-              src="/D5/16.jpg"
-              alt="Health Program"
-              className="w-full h-48 object-cover"
-            />
-            <div className="p-6">
-              <h3 className="text-xl font-semibold text-gray-700">
-                Physical Health Program
-              </h3>
-              <p className="text-gray-500 mt-2">
-                Our physical health program focuses on improving fitness levels,
-                providing nutritional guidance, and offering therapeutic
-                services to help veterans regain and maintain their physical
-                health.
-              </p>
-            </div>
-          </div>
-
-          <div className="border border-gray-200 rounded-lg overflow-hidden shadow-lg bg-gray-100">
-            <img
-              src="/D5/17.jpg"
-              alt="Mental Health Support"
-              className="w-full h-48 object-cover"
-            />
-            <div className="p-6">
-              <h3 className="text-xl font-semibold text-gray-700">
-                Mental Health Support
-              </h3>
-              <p className="text-gray-500 mt-2">
-                We provide mental health support through counseling, therapy,
-                and stress management techniques to ensure veterans have the
-                resources they need for mental well-being.
-              </p>
-            </div>
-          </div>
-
-          <div className="border border-gray-200 rounded-lg overflow-hidden shadow-lg bg-gray-100">
-            <img
-              src="/D5/15.jpg"
-              alt="Community Engagement"
-              className="w-full h-48 object-cover"
-            />
-            <div className="p-6">
-              <h3 className="text-xl font-semibold text-gray-700">
-                Community Engagement
-              </h3>
-              <p className="text-gray-500 mt-2">
-                Our community engagement initiatives are designed to build a
-                supportive network, foster camaraderie, and create opportunities
-                for veterans to connect and thrive.
-              </p>
-            </div>
-          </div>
-        </section>
-
-        <section className="text-center">
-          <a
-            href="#contact"
-            className="bg-gray-100 text-blue-500 py-3 px-6 rounded-lg hover:bg-blue-700 transition duration-300"
-          >
-            Get In Touch
-          </a>
-        </section>
-      </main>
     </div>
   );
 }
